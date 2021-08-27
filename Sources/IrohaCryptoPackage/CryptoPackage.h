716f05e0b0690d15826ad63db849353eaad7c990
lishuailibertine <lishuai19@yeah.net>
2021-08-27 18:02:53 +0800

    fix

---
+++ Sources/IrohaCryptoPackage/CryptoPackage.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  
+//
+//  Created by li shuai on 2021/8/27.
+//
+
+#ifndef Header_h
+#define Header_h
+
+@import Bip39;
+@import CSecp256k1;
+@import Sr25519;
+@import UncommonCrypto;
+#endif /* Header_h */
